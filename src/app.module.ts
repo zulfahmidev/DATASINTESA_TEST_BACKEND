0e52a0efede63fe59b564f43c78899a1692830da
zulfahmidev <developer.zulfahmi@gmail.com>
2025-09-13 08:40:10 +0700

    feat: add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-import { RawDataService } from './raw-data/raw-data.service';
-import { RawDataController } from './raw-data/raw-data.controller';
 import { RawDataModule } from './raw-data/raw-data.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/mydb'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: `mongodb://${configService.get<string>('DB_HOST')}:${configService.get<string>('DB_PORT')}/${configService.get<string>('DB_NAME')}`,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+    }),
     RawDataModule
   ],
 })
